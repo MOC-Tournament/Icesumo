8bd3b7d5c291dc28ae913ee1f399f81159721afc
Andy Shen <andyshen2006@outlook.com>
2025-07-13 21:56:01 +0800

    Fix: 修正若干问题 1.解决游戏结束后玩家背包未清空，未传送到指定位置等问题 2.解决UniversalDataManager.clear()滥用问题 3.解决部分异步问题 4.提供更多调试信息

--- src/main/kotlin/top/andyshen2006/icesumo/ControlFlowUtils.kt
+++ src/main/kotlin/top/andyshen2006/icesumo/ControlFlowUtils.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import org.bukkit.Bukkit
 import org.bukkit.GameMode
@@ -20,6 +21,7 @@ import org.bukkit.event.player.PlayerMoveEvent
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 
+
 class StartCommandExecutor : CommandExecutor, Listener {
     @Suppress("Deprecation", "UnstableApiUsage")
     override fun onCommand(
@@ -29,20 +31,23 @@ class StartCommandExecutor : CommandExecutor, Listener {
         args: Array<out String>?
     ): Boolean {
         val plugin = Bukkit.getPluginManager().getPlugin("IceSumo") as Icesumo
-        when{
+        when {
             !sender.hasPermission("icesumo.referee") -> {
-                MessageUtils.sendMessage(sender,"你没有执行该命令的权限：该命令只允许裁判员执行")
+                MessageUtils.sendMessage(sender, "你没有执行该命令的权限：该命令只允许裁判员执行")
                 return false
             }
+
             !UniversalDataManager.isListValid() -> {
-                MessageUtils.sendMessage(sender,"人数不符合要求，无法启动比赛")
+                MessageUtils.sendMessage(sender, "人数不符合要求，无法启动比赛")
                 return false
             }
-            UniversalDataManager.isRuleValid()!="T" -> {
-                MessageUtils.sendMessage(sender,"服务器规则不符合要求，无法启动比赛")
+
+            UniversalDataManager.isRuleValid() != "T" -> {
+                MessageUtils.sendMessage(sender, "服务器规则不符合要求，无法启动比赛")
             }
+
             UniversalDataManager.start() -> {   //规则请参见：https://moc.miraheze.org/wiki/?curid=452
-                MessageUtils.sendMessage(sender,"正在启动比赛……")
+                MessageUtils.sendMessage(sender, "正在启动比赛……")
                 for (i in 0..(UniversalDataManager.checkinList.size - 1)) {  //自动传送
                     val player = UniversalDataManager.checkinList[i]
                     val stadiumPos = UniversalDataManager.getStadiumPos(i)
@@ -55,8 +60,8 @@ class StartCommandExecutor : CommandExecutor, Listener {
                         player.pitch
                     )
                     player.teleport(location)
-                    player.gameMode= GameMode.ADVENTURE //设置为冒险模式
-                    player.activePotionEffects.forEach{ //移除所有效果
+                    player.gameMode = GameMode.ADVENTURE //设置为冒险模式
+                    player.activePotionEffects.forEach { //移除所有效果
                         player.removePotionEffect(it.type)
                     }
 //                    Attribute.entries.forEach {attribute -> //移除所有属性
@@ -97,42 +102,64 @@ class StartCommandExecutor : CommandExecutor, Listener {
                     player.addPotionEffect(PotionEffect(PotionEffectType.RESISTANCE, 3600, 5, false))   //抗性提升
                     player.addPotionEffect(PotionEffect(PotionEffectType.REGENERATION, 3600, 5, false)) //生命恢复
                     player.addPotionEffect(PotionEffect(PotionEffectType.SATURATION, 3600, 5, false))
-                    player.health=player.maxHealth
+                    player.health = player.maxHealth
 //                    player.saturation=20.0.toFloat()
                     Bukkit.getLogger().info("Giving Kit to ${player.name} now")
                     //Bukkit.getLogger().info("Status ${Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
                     //    "sudo command ${player.name} kit icesumo")}")
                     Bukkit.getLogger().info(
-                        """Status ${Bukkit.dispatchCommand(
-                            Bukkit.getConsoleSender(),
-                            """give ${player.name} diamond_hoe[lore=['[""]','["",{"text":"竞赛用品","italic":false,"bold":true,"color":"green"}]'],enchantments={levels:{knockback:2}},unbreakable={}]"""
-                        )}"""
+                        """Status ${
+                            Bukkit.dispatchCommand(
+                                Bukkit.getConsoleSender(),
+                                """give ${player.name} diamond_hoe[lore=['[""]','["",{"text":"竞赛用品","italic":false,"bold":true,"color":"green"}]'],enchantments={levels:{knockback:2}},unbreakable={}]"""
+                            )
+                        }"""
                     )
                 }
-                UniversalDataManager.world.time=6000    //调整为白天
+                UniversalDataManager.world.time = 6000    //调整为白天
                 Bukkit.getScheduler().runTask(plugin, Runnable {
                     CoroutineScope(Dispatchers.Default).launch {    //协程，启动！
                         val countdownJob = UniversalDataManager.checkinList.map { player ->            //赛前倒计时
                             async { countdownBeforeGame(player) }
                         }
                         countdownJob.awaitAll()
-                        UniversalDataManager.preparing=false
-                        val gameJob=listOf(::allOnlineCheck, ::winningCheck, ::countdown,::onFailPlayerLeave).map { check ->
-                            async { check() }
-                        }
-                        gameJob.awaitAll()
-                        UniversalDataManager.checkinList.forEach { player ->
-                            player.activePotionEffects.forEach{ //移除所有效果
-                                player.removePotionEffect(it.type)
+                        UniversalDataManager.preparing = false
+                        val gameJob =
+                            listOf(::allOnlineCheck, ::winningCheck, ::countdown, ::onFailPlayerLeave).map { check ->
+                                launch { check() }
                             }
-                            player.inventory.clear()    //清空背包
-                        }
-                        UniversalDataManager.clear()
+                        gameJob.joinAll()
+//                        gameJob.awaitAll()
+//                        while (UniversalDataManager.isStart) {  //阻塞直到任何一个异步宣布比赛结束
+//                            Bukkit.getLogger().info("Pending!")
+//                            delay(1000)
+//                        }
+                        Bukkit.getLogger().info("Resetting all players now")
+                        Bukkit.getScheduler().runTask(plugin, Runnable {
+                            UniversalDataManager.checkinList.forEach { player ->
+                                player.activePotionEffects.forEach { //移除所有效果
+                                    player.removePotionEffect(it.type)
+                                }
+                                val location = Location(
+                                    UniversalDataManager.world,
+                                    UniversalDataManager.gravePos.first,
+                                    UniversalDataManager.gravePos.second,
+                                    UniversalDataManager.gravePos.third,
+                                    player.yaw,
+                                    player.pitch
+                                )
+                                player.teleport(location)   //传送到坟墓位置
+                                player.inventory.clear()    //清空背包
+                            }
+                            UniversalDataManager.clear()
+                        })
+
                     }
                 })
             }
+
             else -> {
-                MessageUtils.sendMessage(sender,"你不能开始一场已经开始的比赛")
+                MessageUtils.sendMessage(sender, "你不能开始一场已经开始的比赛")
                 return false
             }
         }
@@ -146,7 +173,7 @@ class StartCommandExecutor : CommandExecutor, Listener {
 
     @Suppress("DEPRECATION")
     suspend fun countdownBeforeGame(player: Player) {   // 协程函数，可以被挂起
-        UniversalDataManager.preparing=true
+        UniversalDataManager.preparing = true
         player.sendTitle("比赛准备开始", "", 10, 20, 10)
         delay(400)
         player.sendTitle("比赛准备开始", "", 10, 20, 10)
@@ -161,7 +188,7 @@ class StartCommandExecutor : CommandExecutor, Listener {
             delay(1000)
         }
         player.sendTitle("比赛开始", "", 10, 20, 10)
-        UniversalDataManager.preparing=false
+        UniversalDataManager.preparing = false
     }
 
     @EventHandler
@@ -173,7 +200,7 @@ class StartCommandExecutor : CommandExecutor, Listener {
     suspend fun winningCheck() {
         //胜利检测
         while (true) {
-            if (UniversalDataManager.isEnd()==1) {
+            if (UniversalDataManager.isEnd() == 1) {
                 var winner: Player? = null
                 UniversalDataManager.checkinList.forEach { player ->
                     if (!UniversalDataManager.isFail(player)) {
@@ -185,18 +212,18 @@ class StartCommandExecutor : CommandExecutor, Listener {
                     player.sendTitle("胜利者为：${winner?.name}", "", 20, 20, 20)//宣布胜利者
                     Bukkit.getLogger().info("胜利者为：${winner?.name}")
                 }
-                CSVManager.appendResult(UniversalDataManager.resultFile!!,winner?.uniqueId.toString(),winner?.name!!,true,
-                    UniversalDataManager.restTime)
+                CSVManager.appendResult(
+                    UniversalDataManager.resultFile!!, winner?.uniqueId.toString(), winner?.name!!, true,
+                    UniversalDataManager.restTime
+                )
                 UniversalDataManager.stop()
-                UniversalDataManager.clear()
                 break
-            }else if(UniversalDataManager.isEnd()==-1) {
+            } else if (UniversalDataManager.isEnd() == -1) {
                 for (player in UniversalDataManager.checkinList) {
                     player.sendTitle("比赛结束", "", 10, 20, 10)
                     player.sendTitle("本轮未决出胜者", "", 20, 20, 20)//宣布未胜利
                 }
                 UniversalDataManager.stop()
-                UniversalDataManager.clear()
                 break
             }
             if (!UniversalDataManager.isStart) {  //比赛已经终止
@@ -204,20 +231,25 @@ class StartCommandExecutor : CommandExecutor, Listener {
             }
             delay(100)
         }
+        Bukkit.getLogger().info("Quitting winningCheck Job!")
     }
 
-    @Suppress("Deprecation")
+    @Suppress("Deprecation","UnstableApiUsage")
     suspend fun countdown() {
-        while (UniversalDataManager.restTime>0) {
-            when{
-                !UniversalDataManager.isStart->break
-                UniversalDataManager.restTime>5-> {
+        while (UniversalDataManager.restTime > 0) {
+            when {
+                !UniversalDataManager.isStart -> {
+                    Bukkit.getLogger().info("Quitting Countdown Job!")
+                    return
+                }
+                UniversalDataManager.restTime > 5 -> {
                     UniversalDataManager.checkinList.forEach { player ->
                         player.sendActionBar("${UniversalDataManager.restTime}")
                     }
                     UniversalDataManager.restTime--
                     delay(1000)
                 }
+
                 else -> {
                     UniversalDataManager.checkinList.forEach { player ->
                         player.sendTitle("${UniversalDataManager.restTime}", "", 10, 20, 10)
@@ -231,17 +263,17 @@ class StartCommandExecutor : CommandExecutor, Listener {
             player.sendTitle("时间到！", "", 10, 20, 10)
             player.sendTitle("比赛结束！", "", 10, 20, 10)
         }
-        if(UniversalDataManager.isEnd()==0) {
+        if (UniversalDataManager.isEnd() == 0) {
             UniversalDataManager.checkinList.forEach { player ->
-                player.sendTitle("比赛结束", "", 10, 20, 10)
                 player.sendTitle("本轮未决出胜者", "", 20, 20, 20)//宣布未胜利
             }
         }
+        Bukkit.getLogger().info("Quitting Countdown Job!")
         UniversalDataManager.stop()//时间到，终止比赛
-        UniversalDataManager.clear()
+
     }
 
-    @Suppress("Deprecation")
+    @Suppress("Deprecation","UnstableApiUsage")
     suspend fun allOnlineCheck() {    //对
         //如果中途有玩家退出->紧急终止
         while (true) {
@@ -250,8 +282,6 @@ class StartCommandExecutor : CommandExecutor, Listener {
                     UniversalDataManager.checkinList.forEach { player ->
                         player.sendTitle("比赛被紧急终止，原因：游戏中途有玩家退出", "", 10, 20, 10)
                     }
-                    UniversalDataManager.stop()    //需要终止游戏
-                    UniversalDataManager.clear()
                 }
             }
             if (!UniversalDataManager.isStart) {  //比赛已经终止，结束监听
@@ -259,13 +289,17 @@ class StartCommandExecutor : CommandExecutor, Listener {
             }
             delay(100)  //间隔2tick再次检测
         }
+        Bukkit.getLogger().info("Quitting allOnlineCheck Job!")
+        UniversalDataManager.stop()
+
     }
 
+    @Suppress("UnstableApiUsage")
     suspend fun onFailPlayerLeave() {
         while (true) {
             UniversalDataManager.failList.forEach { player ->
                 if (!player.isOnline && !UniversalDataManager.isFail(player)) {   //结束玩家+中途退出
-                    UniversalDataManager.failList.removeIf { it.uniqueId==player.uniqueId }
+                    UniversalDataManager.failList.removeIf { it.uniqueId == player.uniqueId }
                 }
             }
             if (!UniversalDataManager.isStart) {  //比赛已经终止，结束监听
@@ -273,25 +307,25 @@ class StartCommandExecutor : CommandExecutor, Listener {
             }
             delay(100)  //间隔2tick再次检测
         }
+        Bukkit.getLogger().info("Quitting onFailPlayerLeave Job!")
     }
 }
 
 class TerminateCommandExecutor : CommandExecutor {
     @Suppress("Deprecation")
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
-        if(!sender.hasPermission("icesumo.referee")) {
-            MessageUtils.sendMessage(sender,"你没有执行该命令的权限：该命令只允许裁判员执行")
+        if (!sender.hasPermission("icesumo.referee")) {
+            MessageUtils.sendMessage(sender, "你没有执行该命令的权限：该命令只允许裁判员执行")
             return false
         }
-        if(UniversalDataManager.isStart) {
+        if (UniversalDataManager.isStart) {
             UniversalDataManager.checkinList.forEach { player ->
                 player.sendTitle("比赛被紧急终止，原因：${args?.get(0)}", "", 10, 20, 10)
             }
-            UniversalDataManager.stop()
-            UniversalDataManager.clear()
-        }else{
+        } else {
             sender.sendMessage("无法终止未开始的比赛")
         }
+        UniversalDataManager.stop()
         return true
     }
 }
